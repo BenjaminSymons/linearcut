242699790ece37e5b8b4145386c24b51c97f47fa
Benjamin <Benjamin-S@users.noreply.github.com>
2021-01-11 08:57:34 +0800

    put the base of the tool in

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <section class="tool"></section>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-};
+export default {};
 </script>
 
 <style lang="scss">
 body {
   width: 100vw;
   min-height: 100vh;
-  background: #192d38; /* Old browsers */
-  background: -moz-radial-gradient(
-    center,
-    ellipse cover,
-    #192d38 0%,
-    #211f2f 100%
-  ); /* FF3.6-15 */
-  background: -webkit-radial-gradient(
-    center,
-    ellipse cover,
-    #192d38 0%,
-    #211f2f 100%
-  ); /* Chrome10-25,Safari5.1-6 */
-  background: radial-gradient(
-    ellipse at center,
-    #192d38 0%,
-    #211f2f 100%
-  ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#192d38', endColorstr='#211f2f',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
-  font-family: "Mukta Mahee", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: white;
-  font-weight: 300;
 }
 
 #app {
+  margin: 5vmin;
+}
+
+.tool {
+  border: 1px solid #08c5ff;
+  width: calc(70vw - 50px);
+  height: calc(90vh - 50px);
+  background: #192d38; /* Old browsers */
+  background: -moz-linear-gradient(
+    top,
+    #16242c 0%,
+    #12101d 100%
+  ); /* FF3.6-15 */
+  background: -webkit-linear-gradient(
+    top,
+    #16242c 0%,
+    #12101d 100%
+  ); /* Chrome10-25,Safari5.1-6 */
+  background: linear-gradient(
+    to bottom,
+    #16242c 0%,
+    #12101d 100%
+  ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#16242c', endColorstr='#12101d',GradientType=0 ); /* IE6-9 fallback on horizontal gradient */
+  box-shadow: 0 2px 20px 0 #000;
 }
 </style>
