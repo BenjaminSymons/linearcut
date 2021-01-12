d331321b3feb643596f2224937a6d3acff745a28
Benjamin <Benjamin-S@users.noreply.github.com>
2021-01-12 12:12:38 +0800

    style updates

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
+    <app-logo/>
     <section class="tool"></section>
   </div>
 </template>
 
 <script>
-export default {};
+import AppLogo from "./components/AppLogo.vue";
+
+export default {
+  components: {
+    AppLogo
+  }
+};
 </script>
 
 <style lang="scss">
 body {
   width: 100vw;
   min-height: 100vh;
+  background: $b1; /* Old browsers */
+  background: -moz-radial-gradient(
+    center,
+    ellipse cover,
+    $b1 0%,
+    $b2 100%
+  ); /* FF3.6-15 */
+  background: -webkit-radial-gradient(
+    center,
+    ellipse cover,
+    $b1 0%,
+    $b2 100%
+  ); /* Chrome10-25,Safari5.1-6 */
+  background: radial-gradient(
+    ellipse at center,
+    $b1 0%,
+    $b2 100%
+  ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='$b1', endColorstr='$b2',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+  font-family: "Mukta Mahee", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #222;
+  font-weight: 300;
 }
 
 #app {
@@ -19,26 +50,26 @@ body {
 }
 
 .tool {
-  border: 1px solid #08c5ff;
+  border: 1px solid $deepSkyBlue;
   width: calc(70vw - 50px);
-  height: calc(90vh - 50px);
-  background: #192d38; /* Old browsers */
+  height: calc(70vh - 50px);
+  background: $t1; /* Old browsers */
   background: -moz-linear-gradient(
     top,
-    #16242c 0%,
-    #12101d 100%
+    $t1 0%,
+    $t2 100%
   ); /* FF3.6-15 */
   background: -webkit-linear-gradient(
     top,
-    #16242c 0%,
-    #12101d 100%
+    $t1 0%,
+    $t2 100%
   ); /* Chrome10-25,Safari5.1-6 */
   background: linear-gradient(
     to bottom,
-    #16242c 0%,
-    #12101d 100%
+    $t1 0%,
+    $t2 100%
   ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
   filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#16242c', endColorstr='#12101d',GradientType=0 ); /* IE6-9 fallback on horizontal gradient */
-  box-shadow: 0 2px 20px 0 #000;
+  box-shadow: 0 2px 20px 0 #AAA;
 }
 </style>
